4d605511487f0714af2fa8f8c78458dc7ac714d9
andresrbaez <andres.baez24@hotmail.com>
2022-06-24 14:35:42 -0500

    subo primer entrega modulo React

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+// import { useState } from 'react'
+import './App.css'
+import QuoteBox from './components/QuoteBox';
+// import quotes from './quotes.json'
+
+const colors = ["#5539A5", "#322F20", "#6A5837", "#988F2A", "#ffc75f", "#C28E70", "#90905C"]
+
+function App() {
+
+  /* --- Generé mucho comentario ya que había realizado el ejercicio en este mismo espacio, preferí no borrarlo para guiarme más adelante --- */
+
+  // const random = Math.floor(Math.random() * quotes.length);
+  // const [ index, setIndex ] = useState(random);
+
+  // const changeQuote = () => {
+  // const random = Math.floor(Math.random() * quotes.length);
+  //   setIndex(random)
+  // }
+
+  // const randomColorIndex = Math.floor(Math.random() * colors.length);
+
+  // const color = colors[randomColorIndex]
+
+  // document.body.style = `background: ${color}`;
+
+  //className="App" style={{ color: color }}
+
+  return (
+    <>
+      <div>
+        <QuoteBox/>
+
+      </div>
+      {/* <div className="App" style={{color: color}}>
+        <div className="container">
+          <div className="card">
+            <div className="quote-autor">
+              <div className="quote">
+                <p>
+                  {quotes[index].quote}
+                </p>
+              </div>
+              <div className="autor">
+                <p>
+                  {quotes[index].author}
+                </p>
+              </div>
+              <div className="btn-cambio">
+              <button style={{backgroundColor: color}} onClick={changeQuote}><i class="fa-solid fa-shuffle"></i></button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/QuoteBox.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import quotes from '../quotes.json';
+import { useState } from 'react'
+
+
+
+
+
+const QuoteBox = () => {
+
+    const colors = ["#5539A5", "#322F20", "#6A5837", "#988F2A", "#ffc75f", "#C28E70", "#90905C"]
+
+
+  const random = Math.floor(Math.random() * quotes.length);
+  const [index, setIndex] = useState(random);
+
+  const changeQuote = () => {
+    const random = Math.floor(Math.random() * quotes.length);
+    setIndex(random);
+  };
+  const randomColorIndex = Math.floor(Math.random() * colors.length);
+
+  const color = colors[randomColorIndex]
+
+  document.body.style = `background: ${color}`;
+
+  return (
+    <div>
+      <div className="App" style={{ color: color }}>
+        <div className="container">
+          <div className="card">
+            <div className="quote-author">
+              <div className="quote">
+              <i class="fa-solid fa-quote-left quote-size"></i>
+                <p>{quotes[index].quote}</p>
+              </div>
+              <div className="author">
+                <p>{quotes[index].author}</p>
+              </div>
+              <div className="btn-cambio">
+                <button
+                  style={{ backgroundColor: color }}
+                  onClick={changeQuote}
+                >
+                  <i class="fa-solid fa-shuffle"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QuoteBox;
